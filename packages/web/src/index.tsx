c1f6425759ea80d14b8ebd005b5a136c4fde4ca7
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-13 16:08:43 -0500

    setup apollo configuratiomn

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { client } from "./apollo";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
+registerServiceWorker();
