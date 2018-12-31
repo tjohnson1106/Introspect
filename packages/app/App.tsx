d0026486e4e9c2b3a3800166d7046c4ca1ce22c4
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-31 16:14:24 -0500

    created initial apollo configuration

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,22 +1,3 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { add } from "@cashew/common";
+import App from "./src/index";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{add(1, 2)}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+export default App;
---
+++ packages/app/src/apollo.ts
@@ -0,0 +1,14 @@
+import { Platform } from "react-native";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const host =
+  Platform.OS === "ios" ? "http://localhost:4000" : "http://10.0.2.2:4000";
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: host
+  }),
+  cache: new InMemoryCache()
+});
