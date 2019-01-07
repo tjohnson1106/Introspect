5f575a92499a7239f73694c4a75948c48c00d977
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-07 15:20:31 -0500

    created on change text method

--- packages/app/src/modules/shared/inputField.tsx
+++ packages/app/src/modules/shared/inputField.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
+import Component = React.Component;
+import { Input } from "react-native-elements";
 import { FieldProps } from "formik";
-import { Form, Input } from "antd";
-
-const FormItem = Form.Item;
 
 // ant design example
 // <div>
@@ -12,21 +11,43 @@ const FormItem = Form.Item;
 // )}
 // </div>
 
-export const InputField: React.SFC<
-  FieldProps<any> & {
-    prefix: React.ReactNode;
+export class InputField extends Component<
+  FieldProps<any> & { name: string }
+> {
+  _onChangeText = (text: string) => {
+    const {
+      form: { setFieldValue },
+      field: { name }
+    } = this.props;
+    // from formik
+    setFieldValue(name, text);
+  };
+
+  render() {
+    const {
+      field,
+      form: { touched, errors },
+      ...props
+    } = this.props;
+    const errorMessage = touched[field.name] && errors[field.name];
+    return (
+      // <FormItem
+      //   help={errorMessage}
+      //   // an error should appear otherwise it's undefined
+      //   validateStatus={errorMessage ? "error" : undefined}
+      // >
+      //   <Input {...field} {...props} />
+      // </FormItem>
+      <Input
+        placeholder=""
+        errorStyle={{ color: "red" }}
+        errorMessage=""
+        onChangeText={this._onChangeText}
+        value={field.value}
+        onBlur={field.onBlur}
+      />
+    );
   }
-> = ({ field, form: { touched, errors }, ...props }) => {
-  const errorMessage = touched[field.name] && errors[field.name];
-  return (
-    <FormItem
-      help={errorMessage}
-      // an error should appear otherwise it's undefined
-      validateStatus={errorMessage ? "error" : undefined}
-    >
-      <Input {...field} {...props} />
-    </FormItem>
-  );
-};
+}
 
 export default InputField;
